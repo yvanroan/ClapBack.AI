40e4ab33ba0379205c8fb65db8c3bd9132ba5d44
Yvan Roan <yvanroan@MacBookAir-de-Yvan.local>
2025-05-04 13:31:45 -0700

    debugging cors in prod

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,5 +5,7 @@ from backend.app import create_application
 app = create_application()
 
 if __name__ == "__main__":
+    print("✅ FRONTEND_URL:", settings.FRONTEND_URL)
+    print("✅ CORS ORIGINS:", settings.BACKEND_CORS_ORIGINS)
     # Run the application with uvicorn when script is executed directly
     uvicorn.run("backend.app.main:app", host="0.0.0.0", port=8000, reload=True) 
